80a9e1275a5cc9eb94f24d6095ada7e707dd43f9
yukikoma <yuki.k.lion@gmail.com>
2022-10-22 23:31:59 +0900

    deploy: section1 ローカルにデプロイするまで完了。
    nodeは立ち上げておく必要がある。

--- wave-portal-koma/scripts/deploy.ts
+++ wave-portal-koma/scripts/deploy.ts
@@ -1,6 +1,16 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
+  const waveContract = await ethers.getContractFactory("WavePortal");
+  const wavePortal = await waveContract.deploy();
+
+  console.log("Deploying contracts with account: ", deployer.address);
+  console.log("Account balance: ", accountBalance.toString());
+  console.log("Contract deployed to: ", wavePortal.address);
+  console.log("Contract deployed by: ", deployer.address);
+
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
   const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
@@ -8,7 +18,7 @@ async function main() {
   const lockedAmount = ethers.utils.parseEther("1");
 
   const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const lock = await Lock.deploy(unlockTime, {value: lockedAmount});
 
   await lock.deployed();
 
